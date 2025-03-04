ea1778283e1eb100ec1155693a87c66625b64a7f
Erik Alkenbrecher <EAlkenbrecher@hvalken.lan>
2025-03-04 17:12:22 +0100

    cleanup refactored scannerview

--- KeyManager/scannerview.cpp
+++ KeyManager/scannerview.cpp
@@ -23,11 +23,6 @@ ScannerView::ScannerView(QWidget *parent)
     : QWidget{parent}
 {
     m_viewfinder = 0;
-    m_DecoderTimer = 0;
-    m_ImageCapture = 0;
-
-    m_DecoderTimer = 0;
-    //m_scanFrequency = 5000;
 
     // main Layout
     QVBoxLayout *layout = new QVBoxLayout;
@@ -43,103 +38,20 @@ ScannerView::ScannerView(QWidget *parent)
 
     layout->setStretch(1, 5);
 
-    m_lastPhoto = new QLabel ("");
-    layout->addWidget(m_lastPhoto);
-
     QPushButton* btnAbort = new QPushButton ("Abbrechen");
     layout->addWidget(btnAbort);
-
-    // m_ImageCapture = new QImageCapture ();
-    // m_captureSession.setImageCapture(m_ImageCapture);
-    // m_ImageCapture->setQuality(QImageCapture::HighQuality);
-    // m_ImageCapture->setFileFormat(QImageCapture::JPEG);
-    //connect(m_ImageCapture, &QImageCapture::imageCaptured, this, &ScannerView::processCapturedImage);
-
-    //m_DecoderTimer = new QTimer (this);
-    //connect (m_DecoderTimer, &QTimer::timeout, this, &ScannerView::takePicture);
 
     connect (btnAbort, SIGNAL (clicked()), this, SLOT (onAbortBtnClicked()));
 }
 
 void ScannerView::onAbortBtnClicked ()
 {
-    stopScanning ();
     emit abortScanner ();
-}
-
-void ScannerView::startScanning ()
-{
-    // if (0 != m_DecoderTimer)
-    // {
-    //     m_DecoderTimer->start ();
-    //     m_DecoderTimer->setInterval(m_scanFrequency);
-    // }
-}
-
-void ScannerView::stopScanning ()
-{
-    // if (0 != m_DecoderTimer)
-    //     m_DecoderTimer->stop ();
 }
 
 QSize ScannerView::getViewfinderSize ()
 {
     return m_viewfinder->size();
-}
-
-void ScannerView::processCapturedImage(int requestId, const QImage &img)
-{
-    return;
-
-    Q_UNUSED(requestId);
-
-    QImage scaledImage = img.scaled(m_viewfinder->size(), Qt::KeepAspectRatio, Qt::FastTransformation);
-
-    QZXing decoder;
-    //mandatory settings
-    decoder.setDecoder(QZXing::DecoderFormat_CODE_128);
-
-    //optional settings
-    decoder.setSourceFilterType(QZXing::SourceFilter_ImageNormal);
-    decoder.setTryHarderBehaviour(QZXing::TryHarderBehaviour_ThoroughScanning | QZXing::TryHarderBehaviour_Rotate);
-
-    m_lastPhoto->setPixmap(QPixmap::fromImage(scaledImage));
-;
-    //trigger decode
-    QString result = decoder.decodeImage(scaledImage);
-
-    qDebug () << "Data: " << result.toStdString();
-
-    if (result.contains("M0100", Qt::CaseInsensitive))
-    {
-        qDebug () << "Success! Code is valid: " << result;
-
-        // code recognized: play a supermarket beep sound :)
-        QMediaPlayer *player = new QMediaPlayer;
-        QAudioOutput *audioOut = new QAudioOutput;
-        player->setAudioOutput(audioOut);
-        QUrl filelocation ("qrc:/sounds/scanner_beep.mp3");
-        player->setSource(filelocation);
-        audioOut->setVolume(100);
-        player->play();
-
-        m_DecoderTimer->stop();
-
-        int code = 50;
-
-        emit codeRetrieved (code);
-    }
-}
-
-void ScannerView::onScanButtonReleased()
-{
-    // if (0 != m_DecoderTimer)
-    // {
-    //     if (m_DecoderTimer->isActive())
-    //         m_DecoderTimer->stop();
-    //     else
-    //         m_DecoderTimer->start (m_scanFrequency);
-    // }
 }
 
 QVideoWidget* ScannerView::getViewfinder ()
@@ -149,42 +61,10 @@ QVideoWidget* ScannerView::getViewfinder ()
     return m_viewfinder;
 }
 
-void ScannerView::setVideoOutput (QMediaCaptureSession *captureSession)
-{
-    if (!captureSession)
-        return;
-
-    qDebug () << "ScannerView::setVideoOutput called";
-    captureSession->setVideoOutput(m_viewfinder);
-
-}
-
-//void ScannerView::setCamera (const QCameraDevice &cameraDevice)
-void ScannerView::setCamera (QCamera *cameraDevice)
-{
-    // if (0 != cameraDevice)
-    // {
-    //     m_captureSession.setCamera(cameraDevice);
-    //     m_captureSession.setVideoOutput(m_viewfinder);
-    //     cameraDevice->start();
-    // }
-}
-
-void ScannerView::takePicture ()
-{
-    // if (m_ImageCapture->isReadyForCapture())
-    //     m_ImageCapture->capture();
-}
-
 ScannerView::~ScannerView()
 {
     if (m_viewfinder != 0)
     {
         delete m_viewfinder;
     }
-
-    // if (m_DecoderTimer != 0)
-    // {
-    //     delete m_DecoderTimer;
-    // }
 }
--- KeyManager/scannerview.h
+++ KeyManager/scannerview.h
@@ -12,53 +12,26 @@
 #include <QImage>
 #include <QSize>
 
-class QTimer;
 class QVideoWidget;
-class QPushButton;
-class QLabel;
 
 class ScannerView : public QWidget
 {
     Q_OBJECT
 public:
     explicit ScannerView(QWidget *parent = nullptr);
-    void setCamera (QCamera *cameraDevice);
-    void startScanning ();
-    void stopScanning ();
-    void setVideoOutput (QMediaCaptureSession *captureSession);
     QVideoWidget* getViewfinder ();
     QSize getViewfinderSize ();
-
     ~ScannerView();
 
 private:
-    void checkAvailableCams ();
-    void checkPermissions ();
-    void takePicture ();
 
-    QMediaDevices m_devices;
-    QImageCapture *m_ImageCapture;
-    QMediaCaptureSession m_captureSession;
     QVideoWidget *m_viewfinder;
 
-    bool m_isCapturingImage = false;
-    bool m_applicationExiting = false;
-    bool m_doImageCapture = true;
-
-    int m_scanFrequency;
-
-    QTimer *m_DecoderTimer;
-    QLabel *m_lastPhoto;
-
 signals:
-    void closeWindow (int returnCode);
-    void codeRetrieved (int code);
     void abortScanner ();
 
 private slots:
     void onAbortBtnClicked ();
-    void onScanButtonReleased ();
-    void processCapturedImage (int requestId, const QImage &img);
 };
 
 #endif // SCANNERVIEW_H
